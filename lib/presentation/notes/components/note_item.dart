d4922626a76eb87ab4f0a95cce8619a21a075cfd
fortuna <avidia38@gmail.com>
2022-10-02 19:02:49 +0900

    feat: note_item

---
+++ lib/presentation/notes/components/note_item.dart
@@ -0,0 +1,59 @@
+import 'package:clean_note_app_2/domain/model/note.dart';
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  final Note note;
+  Function? onDeleteTap;
+
+  NoteItem({
+    Key? key,
+    required this.note,
+    this.onDeleteTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color(note.color),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  note.title,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  note.content,
+                  maxLines: 10,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+              bottom: 8,
+              right: 8,
+              child: GestureDetector(
+                onTap: () {
+                  onDeleteTap?.call();
+                },
+                child: const Icon(Icons.delete),
+              )),
+        ],
+      ),
+    );
+  }
+}
