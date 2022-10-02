4022ebd992dd8e0b18a2d2d41fbb46c8253cd42d
fortuna <avidia38@gmail.com>
2022-10-02 19:28:47 +0900

    feat: add_edit_note_screen & refactor: FAB in  notes_screen

---
+++ lib/presentation/add_edit_note/add_edit_note_screen.dart
@@ -0,0 +1,118 @@
+import 'package:clean_note_app_2/ui/colors.dart';
+import 'package:flutter/material.dart';
+
+class AddEditNoteScreen extends StatefulWidget {
+  const AddEditNoteScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddEditNoteScreen> createState() => _AddEditNoteScreenState();
+}
+
+class _AddEditNoteScreenState extends State<AddEditNoteScreen> {
+
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
+
+  final List<Color> noteColors = [
+    roseBud,
+    primrose,
+    wisteria,
+    skyBlue,
+    illusion,
+  ];
+
+  Color _color = roseBud;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        padding: const EdgeInsets.fromLTRB(16, 60, 16, 16),
+        color: _color,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: noteColors
+                  .map(
+                    (color) => InkWell(
+                  onTap: () {
+                    setState(() {
+                      _color = color;
+                    });
+                  },
+                  child: _buildBackgroundColor(
+                    color: color,
+                    selected: _color == color,
+                  ),
+                ),
+              )
+                  .toList(),
+            ),
+            TextField(
+              controller: _titleController,
+              maxLines: 1,
+              style: Theme.of(context).textTheme.headline5!.copyWith(
+                color: darkGray,
+              ),
+              decoration: const InputDecoration(
+                hintText: '제목을 입력하세요',
+                border: InputBorder.none,
+              ),
+            ),
+            TextField(
+              controller: _contentController,
+              maxLines: null,
+              style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                color: darkGray,
+              ),
+              decoration: const InputDecoration(
+                hintText: '내용을 입력하세요',
+                border: InputBorder.none,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.save),
+      ),
+    );
+  }
+
+  Widget _buildBackgroundColor({
+    required Color color,
+    required bool selected,
+  }) {
+    return Container(
+      width: 48,
+      height: 48,
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 5.0,
+            spreadRadius: 1.0,
+          ),
+        ],
+        border: selected
+            ? Border.all(
+          color: Colors.black,
+          width: 2.0,
+        )
+            : null,
+      ),
+    );
+  }
+}
--- lib/presentation/notes/notes_screen.dart
+++ lib/presentation/notes/notes_screen.dart
@@ -1,3 +1,7 @@
+import 'package:clean_note_app_2/domain/model/note.dart';
+import 'package:clean_note_app_2/presentation/add_edit_note/add_edit_note_screen.dart';
+import 'package:clean_note_app_2/presentation/notes/components/note_item.dart';
+import 'package:clean_note_app_2/ui/colors.dart';
 import 'package:flutter/material.dart';
 
 class NotesScreen extends StatelessWidget {
@@ -5,6 +9,49 @@ class NotesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your note', style: TextStyle(fontSize: 30),),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.sort),
+          ),
+        ],
+        elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder:(context) => const AddEditNoteScreen()),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            NoteItem(
+              note: Note(
+                title: 'title1',
+                content: 'content1',
+                color: wisteria.value,
+                timestamp: 1,
+              ),
+            ),
+            NoteItem(
+              note: Note(
+                title: 'title2',
+                content: 'content2',
+                color: skyBlue.value,
+                timestamp: 2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
